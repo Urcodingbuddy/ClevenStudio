965be8b5d97ab16fbf8748f98c1e7251084fe076
Urcodingbuddy <anshpethe110@gmail.com>
2025-05-03 11:06:58 +0530

    Protected (home) pages

--- apps/client/app/(home)/layout.tsx
+++ apps/client/app/(home)/layout.tsx
@@ -1,8 +1,13 @@
 import type { ReactNode } from "react"
 import { ThemeProvider } from "@repo/components/ui/theme-provider"
 import Sidebar from "@repo/components/ui/sideBar"
+import { authOptions } from "../api/auth/authOptions";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default async function RootLayout({ children }: { children: ReactNode }) {
+  const session = await getServerSession(authOptions);
+  if(!session) redirect('/signin');
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <div className="flex h-screen overflow-hidden bg-[#0c0c0c] text-slate-200">
